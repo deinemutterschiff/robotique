b678e90053a018325927adf6ef906503481f918b
Sylvain <slott@student.42lausanne.ch>
2023-03-03 12:10:12 +0100

    lol

--- test.py
+++ test.py
@@ -4,6 +4,9 @@ MY_IP = '192.168.2.211'
 robot = wrapper.get_robot
 counter = 0
 
+robot.init_sensors()
+robot.calibrate_prox()
+
 while robot.go_on() and counter < 400:
     counter += 1
     if counter % 80 < 40:
